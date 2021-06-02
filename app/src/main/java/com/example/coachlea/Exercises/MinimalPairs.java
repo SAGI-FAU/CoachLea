cf10ceabff7b01c3cbd57f11f4ce8b9b25927a2c
paula-sfr <79749186+paula-sfr@users.noreply.github.com>
2021-06-02 10:28:25 +0200

    Minimal pairs first images added & setMinimal_airs_all

--- app/src/main/java/com/example/coachlea/Exercises/MinimalPairs.java
+++ app/src/main/java/com/example/coachlea/Exercises/MinimalPairs.java
@@ -8,11 +8,19 @@ import com.example.coachlea.R;
 
 public class MinimalPairs extends AppCompatActivity {
 
-    private int[] minimal_pairs =new int[8];
+    private int[] minimal_pairs_all;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.minimal_pairs);
+
+        setMinimal_pairs_all();
+    }
+
+    private void setMinimal_pairs_all(){
+        minimal_pairs_all = new int[]{R.drawable.gold, R.drawable.geld, R.drawable.sand, R.drawable.wand, R.drawable.see, R.drawable.tee, R.drawable.hose,
+                R.drawable.rose, R.drawable.huhn, R.drawable.hut, R.drawable.baum, R.drawable.schaum, R.drawable.geld, R.drawable.welt, R.drawable.tasche, R.drawable.tasse};
     }
 }
