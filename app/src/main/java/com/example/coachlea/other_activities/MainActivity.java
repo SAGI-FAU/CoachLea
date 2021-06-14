e13a5124be8230bad6140230bc8534aa0e1a2c80
paula-sfr <79749186+paula-sfr@users.noreply.github.com>
2021-06-14 09:09:29 +0200

    File writer works now

--- app/src/main/java/com/example/coachlea/other_activities/MainActivity.java
+++ app/src/main/java/com/example/coachlea/other_activities/MainActivity.java
@@ -1,14 +1,19 @@
+
 package com.example.coachlea.other_activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.coachlea.R;
 
 public class MainActivity extends AppCompatActivity {
+    private int All_Code = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void profileButtonClick(View view){
+        requestAll();
+        /*
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestAll();
+        }
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(this, ExercisesMain.class);
+            startActivity(intent);
+        }
+                */
         //TODO
+    }
+
+    //Ask permissions
+    private void requestAll() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+            new androidx.appcompat.app.AlertDialog.Builder(this).setTitle(getResources().getString(R.string.needed)).setMessage(getResources().getString(R.string.permissions)).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, All_Code);
+                }
+            }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).create().show();
+        } else {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, All_Code);
+        }
     }
 }
