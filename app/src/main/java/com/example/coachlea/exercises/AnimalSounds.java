e3e2fea49dc4ea5d85a500bc360b47282a1c844e
paula-sfr <79749186+paula-sfr@users.noreply.github.com>
2021-06-17 15:05:37 +0200

    images AnimalSounds added

--- app/src/main/java/com/example/coachlea/exercises/AnimalSounds.java
+++ app/src/main/java/com/example/coachlea/exercises/AnimalSounds.java
@@ -1,6 +1,8 @@
 package com.example.coachlea.exercises;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -8,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.coachlea.R;
 import com.example.coachlea.data_access.SpeechRecorder;
+import com.example.coachlea.other_activities.MainActivity;
 
 public class AnimalSounds extends AppCompatActivity {
 
@@ -27,9 +30,21 @@ public class AnimalSounds extends AppCompatActivity {
 
         //recorder = SpeechRecorder.getInstance(this, new AnimalSounds().VolumeHandler(), "AnimalSounds"); // TODO Volume Handler
 
-       // animals[0] = R.drawable.a;
+        animals[0] = R.drawable.affe;
+        animals[1] = R.drawable.esel;
+        animals[2] = R.drawable.hund;
+        animals[3] = R.drawable.katze;
 
         imageView.setImageResource(animals[0]);
+
+        home.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                Intent intent = new Intent(AnimalSounds.this, MainActivity.class);
+                v.getContext().startActivity(intent);
+
+            }
+        });
     }
 
 
