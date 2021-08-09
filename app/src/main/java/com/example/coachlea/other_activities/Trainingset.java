08bd7998655adc7e91d715b87360432142e73906
paula-sfr <79749186+paula-sfr@users.noreply.github.com>
2021-08-09 10:51:04 +0200

    trainingset layouts added

---
+++ app/src/main/java/com/example/coachlea/other_activities/Trainingset.java
@@ -0,0 +1,16 @@
+package com.example.coachlea.other_activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coachlea.R;
+
+public class Trainingset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trainingset);
+    }
+}
---
+++ app/src/main/java/com/example/coachlea/other_activities/Trainingset_Finished.java
@@ -0,0 +1,16 @@
+package com.example.coachlea.other_activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coachlea.R;
+
+public class Trainingset_Finished extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trainingset__finished);
+    }
+}
